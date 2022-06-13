242594eec6356e10d43b0504889cb7029e8f757b
vaivodeieva <87878157+deieva@users.noreply.github.com>
2022-06-13 22:07:51 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Views/Home';
+// import Header from './Components/Header';
+// import Footer from './Components/Footer';
 
 function App() {
   return (
     <div>
-     
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
